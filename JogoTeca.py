53df5089b418265d7f594a3cae7c02d3499346be
gugu200123 <play7gameplays@gmail.com>
2024-02-16 21:27:49 -0300

    Atualização 0.1

--- JogoTeca.py
+++ JogoTeca.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
+
+class Jogo:
+    def __init__(self,nome,categoria,console):
+        self.nome=nome
+        self.categoria=categoria
+        self.console=console
+
 
 app = Flask(__name__)
 
 
 @app.route('/inicio')
 def ola():
-    return '<h1>Olá mundo<h1>'
+    jogo1 = Jogo('Tetris','Puzzle','Atari')
+    jogo2 = Jogo('Dota 2','Moba','PC')
+    jogo3 = Jogo('God Of War', 'Ação', 'PS4')
+
+    lista =[jogo1,jogo2,jogo3]
+    return render_template('lista.html', titulo_da_pagina_inicio='Jogos', jogos=lista)
 
 
 app.run()
